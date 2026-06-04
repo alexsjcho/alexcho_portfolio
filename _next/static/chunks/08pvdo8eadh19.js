e94c038a52ae2592f0ae9e4c97aad02c38cecda9
Alex <Alex@Sophies-MacBook-Air.local>
2026-06-04 11:35:31 -0700

    Updates

--- _next/static/chunks/08pvdo8eadh19.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/404", [
-  "static/chunks/1844n_c7eya4c.js",
-  "static/chunks/0f3.ylta0kdkz.js",
-  "static/chunks/turbopack-11-xpt1r4cy-..js"
-])
--- _next/static/chunks/0oj0j2la_.946.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/0ary0880_3w~f.js",
-  "static/chunks/0f3.ylta0kdkz.js",
-  "static/chunks/turbopack-0tz6ieqfpqj0p.js"
-])
